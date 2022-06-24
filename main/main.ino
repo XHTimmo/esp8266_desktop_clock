398fb1d80d80d80cebe5f6910347e037a68133f1
XH_Timmo <slwhx2295317870@outlook.com>
2022-06-24 11:17:41 +0800

    接线图更新

--- main/main.ino
+++ main/main.ino
@@ -1,26 +1,9 @@
-#include <ArduinoWiFiServer.h>
-#include <BearSSLHelpers.h>
-#include <CertStoreBearSSL.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiGratuitous.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiType.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFiClientSecureBearSSL.h>
-#include <WiFiServer.h>
-#include <WiFiServerSecure.h>
-#include <WiFiServerSecureBearSSL.h>
-#include <WiFiUdp.h>
+#include <TFT_eSPI.h>
 
 void setup()
 {
   Serial.begin(115200);                     //波特率115200
-  Serial.println("");
+/*  Serial.println("");
   WiFi.mode(WIFI_STA);
   WiFi.begin("Timmo5","SLwhx...0103");
   while (WiFi.status() != WL_CONNECTED) {   //等待连接wifi
@@ -30,11 +13,12 @@ void setup()
   Serial.println("Connected Successful!!"); //成功连接
     Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
+*/
 }
 
 
 void loop()
 {
+    digitalWrite(TFT_BL, 255);
 }
 
